242066ba7a7b139437da4f2c8c26850a0477d8c8
nilooy <niloy@pathable.com>
2021-07-21 23:42:03 +0200

    :bug: Fix craco commands

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>
-Pokemon
+        <h1 className="text-red-500">
+                Pokemon
         </h1>
     </div>
   );
